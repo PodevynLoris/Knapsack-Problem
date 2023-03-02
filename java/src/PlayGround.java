bccd0aa022495fe4c983d8afca1774eac5314279
PodevynLoris <115578089+PodevynLoris@users.noreply.github.com>
2023-03-02 12:38:23 +0100

    Change Sim to PG

--- java/src/PlayGround.java
+++ java/src/PlayGround.java
@@ -1,8 +1,6 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class Simulation {
+public class PlayGround {
 
     public static void main(String[] args) {
 
